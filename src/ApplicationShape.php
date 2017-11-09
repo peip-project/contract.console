b2871ee439612a6ed3c12676ce096089f3344582
Timo Michna <timomichna@yahoo.de>
2017-11-09 20:38:06 +0100

    add ApplicationShape

---
+++ src/ApplicationShape.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the semtic package.
+ *  (c) ${THIS_YEAR} Timo Michna <timomichna/yahoo.de>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PEIP\Contract\Console;
+
+/**
+ * Interface PEIP\Contract\Console\ApplicationShape.
+ *
+ */
+interface ApplicationShape
+{
+    public function run();
+}
