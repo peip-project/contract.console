479cd69867596a76a4733390b6b1e5876e32b5cd
Timo Michna <timomichna@yahoo.de>
2017-11-09 20:54:43 +0100

    add CommandShape

---
+++ src/CommandShape.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the peip/contract.console package.
+ *  (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PEIP\Contract\Console;
+
+
+/**
+ * Interface PEIP\Contract\Console\CommandShape.
+ *
+ */
+interface CommandShape
+{
+
+}
